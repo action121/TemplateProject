eca7123acf5d402b89681d72263c75527ce0c172
吴晓明 <passion_wxm@163.com>
2019-05-23 09:56:57 +0800

    init

---
+++ templates/ios/Example/PROJECT/CPDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPDAppDelegate.h
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_DATE.
+//  Copyright (c) TODAYS_YEAR ORGANIZATION_NAME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ templates/ios/Example/PROJECT/CPDViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPDViewController.h
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_DATE.
+//  Copyright (c) TODAYS_YEAR ORGANIZATION_NAME. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CPDViewController : UIViewController
+
+@end
